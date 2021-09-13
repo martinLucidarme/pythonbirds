329e72b2120ef0ee3443d0c59687f3b65f4dab6b
martinLucidarme <martinlucidarme@gmail.com>
2021-09-13 16:22:19 -0300

    Atributos complexos para init

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome= None, idade=25):
+    def __init__(self, *filhos, nome= None, idade=25):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p=Pessoa('Martin')
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
-    p.nome = 'oloco'
-    print(p.nome)
+    clayton = Pessoa(nome='Clayton')
+    luciano = Pessoa(clayton, nome='Martin')
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
 
