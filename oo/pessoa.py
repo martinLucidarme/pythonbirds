402151717b243984fdd35e3ad0b008b211b34bf3
martinLucidarme <martinlucidarme@gmail.com>
2021-09-13 16:27:59 -0300

    Criar e remover atrib dinamicos de objeto do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,12 +9,16 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    clayton = Pessoa(nome='Clayton')
+    clayton = Pessoa(nome='Zé')
     luciano = Pessoa(clayton, nome='Martin')
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
-
-
+    luciano.sobrenome= 'Ruella'
+    print(luciano.sobrenome)
+    print(luciano.__dict__)
+    print(clayton.__dict__)
+    del luciano.filhos
+    print(luciano.__dict__)
