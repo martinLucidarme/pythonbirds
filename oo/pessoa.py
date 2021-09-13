fa44a95f052a6831cbf0a479e25c445cbd4b74f1
martinLucidarme <martinlucidarme@gmail.com>
2021-09-13 16:10:00 -0300

    metodo __init__ parametro de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,17 @@
 class Pessoa:
+    def __init__(self, nome= None, idade=25):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'olá {id(self)}'
+
+if __name__ == '__main__':
+    p=Pessoa('Martin')
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+    p.nome = 'oloco'
+    print(p.nome)
+
 
